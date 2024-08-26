25138ad134d35623eb937b6d0ad441c3d1de124b
lethabomichelle <lethabomichelle@gmail.com>
2024-08-26 19:27:48 +0200

    connected frontend with backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Register from './components/register';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
 
     </div>
   );
--- src/components/register.js
+++ src/components/register.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import myImage from './images/sideimage.jpeg';
 import logo from './images/logo.png';
 import glogo from './images/glogo.png';
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
+    const navigate = useNavigate();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const user = { name, email, password }
+
+        const url = 'http://localhost:3500/api/users/'
+        return fetch(url, {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(user)
+        }).then(async res => {
+            res = await res.json();
+            // console.log("res", re s);
+            // navigate("/profile");
+        });
+
+    }
+
     return (
         <div className="register">
             <Row>
@@ -23,13 +46,13 @@ export default function Register() {
                         </button>
                         <div className="separation">Or</div>
                         <form>
-                            <input type="text" name="name" placeholder="Name" required />
-                            <input type="email" name="email" placeholder="Email" required />
-                            <input type="password" name="password" placeholder="Password" required />
+                            <input type="text" name="name" placeholder="Name" required value={name} onChange={(e) => setName(e.nativeEvent.target.value)} />
+                            <input type="email" name="email" placeholder="Email" required value={email} onChange={(e) => setEmail(e.nativeEvent.target.value)} />
+                            <input type="password" name="password" placeholder="Password" required value={password} onChange={(e) => setPassword(e.nativeEvent.target.value)} />
                             <label>
                                 <input type="checkbox" name="remember-me" /> Remember Me
                             </label>
-                            <button type="submit">Register</button>
+                            <button type="submit" onClick={handleSubmit}>Register</button>
                         </form>
                         <p>Already have an account? <a href="/">Log in</a></p>
                     </div>
@@ -40,8 +63,6 @@ export default function Register() {
                     </div>
                 </Col>
             </Row>
-
-
         </div>
     )
 }
